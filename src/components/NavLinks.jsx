588745f56e413430ce0fdd0d2c403acedaf513e1
Hasan Aasim <hasan.asim14@gmail.com>
2025-07-14 12:08:34 +0500

    Test changes

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import './NavLinks.css';
+import "./NavLinks.css";
 
 const NavLinks = () => {
   return (
     <nav className="navlinks-container">
-      <a href="#" className="navlink">Home</a>
-      <a href="#" className="navlink">Courses</a>
-      <a href="#" className="navlink">About</a>
-      <a href="#" className="navlink">Contact</a>
+      <a href="#" className="navlink">
+        Home
+      </a>
+      <a href="#" className="navlink">
+        Courses
+      </a>
+      <a href="#" className="navlink">
+        About
+      </a>
+      <a href="#" className="navlink">
+        Contact
+      </a>
     </nav>
   );
 };
